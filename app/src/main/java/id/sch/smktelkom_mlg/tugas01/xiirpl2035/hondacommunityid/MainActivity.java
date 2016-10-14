f9c8bdf46140a2545194e4136f88588de7ec3a31
sandy <sandyargya123@gmail.com>
2016-10-15 03:03:05 +0700

    Initial commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl2035/hondacommunityid/MainActivity.java
@@ -0,0 +1,16 @@
+package id.sch.smktelkom_mlg.tugas01.xiirpl2035.hondacommunityid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
